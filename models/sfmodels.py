Bug fix in #10. Automatically compute feedback gradients

models/sfmodels.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 from numpy import random as rng
 import numpy as np 
 
+from utils.utils import matmul_fa
+
 class BPModel(BaseModel):
     def __init__(self, config):
         super(BPModel, self).__init__(config)
@@ -53,7 +55,7 @@ class FAModel(BaseModel):
         #Plus one for bias terms
         A = tf.Variable(rng.randn(p+1,m)*alpha0, name="hidden_weights", dtype=tf.float32)
         W = tf.Variable(rng.randn(m+1,n)*alpha1, name="output_weights", dtype=tf.float32)
-        B = tf.Variable(rng.randn(m,n)*alpha2, name="feedback_weights", dtype=tf.float32)
+        B = tf.Variable(rng.randn(m+1,n)*alpha2, name="feedback_weights", dtype=tf.float32)
 
         # network architecture with ones added for bias terms
         e0 = tf.ones([self.config.batch_size, 1], tf.float32)
@@ -61,26 +63,21 @@ class FAModel(BaseModel):
         x_aug = tf.concat([self.x, e0], 1)
         h = tf.sigmoid(tf.matmul(x_aug, A))
         h_aug = tf.concat([h, e1], 1)
-        y_p = tf.matmul(h_aug, W)
+        y_p = matmul_fa(h_aug, W, B)
 
         with tf.name_scope("loss"):
             #mean squared error
             #cost = tf.reduce_sum(tf.pow(y_p-self.y, 2))/2/self.config.batch_size
             self.loss = tf.reduce_sum(tf.pow(y_p-self.y, 2))/2
             grad_W = tf.gradients(xs=W, ys=self.loss)[0]
+            grad_A = tf.gradients(xs=A, ys=self.loss)[0]
 
             e = (y_p - self.y)
-            h_prime = h*(1-h)
-            #BP
-            #grad_A = tf.gradients(xs=A, ys=cost)[0]
-            #grad_A = tf.matmul(tf.transpose(x_aug), tf.multiply(h_prime, tf.matmul(e, tf.transpose(W[0:m,:]))))
-            #FA
-            grad_A = tf.matmul(tf.transpose(x_aug), tf.multiply(h_prime, tf.matmul(e, tf.transpose(B))))
 
             #Feedback data for saving
             #Only take first item in epoch
             delta_bp = tf.matmul(e, tf.transpose(W[0:m,:]))[0,:]
-            delta_fa = tf.matmul(e, tf.transpose(B))[0,:]
+            delta_fa = tf.matmul(e, tf.transpose(B[0:m,:]))[0,:]
             norm_W = tf.norm(W)
             norm_B = tf.norm(B)
             error_FA = tf.norm(delta_bp - delta_fa)

tests/test_matmul_override.py
@@ -2,7 +2,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
 import tensorflow as tf
-from utils.utils import matmul_fa
+from utils.utils import tf_matmul_r, tf_matmul_l
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import numpy.random as rng
@@ -54,16 +54,18 @@ grad_A_manual = tf.matmul(tf.transpose(x_aug), tf.multiply(h_prime_man, tf.matmu
 
 #FA, computed automatically
 x_aug = tf.concat([x, e0], 1)
-h = tf.sigmoid(matmul_fa(x_aug, A, A))
+h = tf.sigmoid(tf.matmul(x_aug, A))
 h_aug = tf.concat([h, e1], 1)
 #The key line! Replace W with B in any backprop step
-y_p = matmul_fa(h_aug, W, B)
+y_p = tf_matmul_r(h_aug, W, B)
+#y_p = tf.matmul(h_aug, W)
 
 loss = tf.reduce_sum(tf.pow(y_p-y, 2))/2
-grad_W = tf.gradients(xs=W, ys=loss)[0]
+grad_W_auto = tf.gradients(xs=W, ys = loss)[0]
 grad_A_auto = tf.gradients(xs=A, ys = loss)[0]
 
-norms = np.zeros(n_tests)
+norms_W = np.zeros(n_tests)
+norms_A = np.zeros(n_tests)
 
 #Compare to overridden matmul functions
 with tf.Session() as sess:
@@ -71,9 +73,15 @@ with tf.Session() as sess:
 	for i in range(n_tests):
 		batch_x, batch_y = mnist.train.next_batch(batch_size)
 		feed_dict = {x: batch_x, y: batch_y}
+		[g_W_auto, g_W_man] = sess.run([grad_W_auto, grad_W_man], feed_dict=feed_dict)
 		[g_A_auto, g_A_man] = sess.run([grad_A_auto, grad_A_manual], feed_dict=feed_dict)
-		norms[i] = np.linalg.norm(g_A_auto - g_A_man, 'fro')/g_A_auto.shape[0]/g_A_auto.shape[1]
+		norms_W[i] = np.linalg.norm(g_W_auto - g_W_man, 'fro')/g_W_auto.shape[0]/g_W_auto.shape[1]
+		norms_A[i] = np.linalg.norm(g_A_auto - g_A_man, 'fro')/g_A_auto.shape[0]/g_A_auto.shape[1]
 
-assert (norms < tol).all(), "Backprop'ed gradients not within tolerance"
+#print norms_W
+#print norms_A
+
+assert (norms_W < tol).all(), "Backprop'ed gradients not within tolerance"
+assert (norms_A < tol).all(), "Backprop'ed gradients not within tolerance"
 
 print "Passes test to within %e."%tol

utils/utils.py
@@ -1,27 +1,68 @@
 import argparse
-
+import numpy as np
+import numpy.random as rng
 import tensorflow as tf
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
 
 def get_args():
-    argparser = argparse.ArgumentParser(description=__doc__)
-    argparser.add_argument(
-        '-c', '--config',
-        metavar='C',
-        default='None',
-        help='The Configuration file')
-    args = argparser.parse_args()
-    return args
+	argparser = argparse.ArgumentParser(description=__doc__)
+	argparser.add_argument(
+		'-c', '--config',
+		metavar='C',
+		default='None',
+		help='The Configuration file')
+	args = argparser.parse_args()
+	return args
 
-def matmul_fa_l(x_f, y_f, x_b):
-	return tf.matmul(x_b,y_f) + tf.stop_gradient(tf.matmul(x_f,y_f) - tf.matmul(x_b,y_f))
+def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
 
-def matmul_fa_r(x_f, y_f, y_b):
-	return tf.matmul(x_f,y_b) + tf.stop_gradient(tf.matmul(x_f,y_f) - tf.matmul(x_f,y_b))
+	# Need to generate a unique name to avoid duplicates:
+	rnd_name = 'PyFuncGrad' + str(np.random.randint(0, 1E+8))
 
-def matmul_fa(x_f, y_f, b):
-	if b.shape == x_f.shape:
-		return matmul_fa_l(x_f, y_f, b)
-	elif b.shape == y_f.shape:
-		return matmul_fa_r(x_f, y_f, b)
-	else:
-		raise ValueError
+	tf.RegisterGradient(rnd_name)(grad)  # see _MySquareGrad for grad example
+	g = tf.get_default_graph()
+	with g.gradient_override_map({"PyFunc": rnd_name}):
+		return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
+
+def np_matmul(A_f,x,A_b):
+	return np.dot(A_f,x).astype(np.float32)
+
+def matmul_l_grad(op, grad):
+	A_f = op.inputs[0]
+	x = op.inputs[1]
+	A_b = op.inputs[2]
+	input_rows = array_ops.shape(x)[0]
+	input_cols = array_ops.shape(x)[1]
+	output_rows = array_ops.shape(A_f)[0]
+	
+	#This is incorrect...
+	#grad_A = x
+	#grad_A = tf.multiply(tf.transpose(grad), tf.reshape(tf.tile(tf.reshape(x,\
+	#					 [input_rows]), [output_rows]), [output_rows, -1]))*0
+	grad_A = tf.matmul(grad, tf.transpose(x))
+
+	#Backprop
+	#grad_x = tf.matmul(tf.transpose(A_f), grad)
+	#Feedback
+	grad_x = tf.matmul(tf.transpose(A_b), grad)
+
+	grad_Ab = grad
+	
+	return [grad_A, grad_x, grad_Ab]
+		
+def tf_matmul_l(A_f,x,A_b, name=None):
+	#A_f = tf.Variable(rng.randn(31,20), name="hidden_weights", dtype=tf.float32)
+	#A_b = tf.Variable(rng.randn(31,20), name="hidden_weights", dtype=tf.float32)
+	#x = tf.Variable(rng.randn(31,1), name="hidden_weights", dtype=tf.float32)
+
+	with ops.name_scope(name, "matmul_fa", [A_f, x, A_b]) as name:
+		a = py_func(np_matmul,
+						[A_f,x,A_b],
+						[tf.float32],
+						name=name,
+						grad=matmul_l_grad)
+		return a[0]
+
+def tf_matmul_r(x,A_f,A_b, name=None):
+	return tf.transpose(tf_matmul_l(tf.transpose(A_f), tf.transpose(x), tf.transpose(A_b)))