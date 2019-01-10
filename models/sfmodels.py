509a6da1d327bd3e707d1f124ad58ca0879f52a7
lansdell <ben.lansdell@gmail.com>
2019-01-10 09:36:02 -0500

    Add feedback alignment and backprop network tests

--- models/sfmodels.py
+++ models/sfmodels.py
@@ -226,12 +226,71 @@ class BPModel4(BaseModel):
         # here you initialize the tensorflow saver that will be used in saving the checkpoints.
         self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
 
+class BPModel4_Small(BaseModel):
+    #Four layers version
+    def __init__(self, config):
+        super(BPModel4_Small, self).__init__(config)
+        self.build_model()
+        #self.init_saver()
+
+    def build_model(self):
+        self.is_training = tf.placeholder(tf.bool)
+        self.x = tf.placeholder(tf.float32, shape=[None] + self.config.state_size)
+        self.y = tf.placeholder(tf.float32, shape=[None, 10])
+
+        # set initial feedforward and feedback weights
+        m = 50
+        j = 20
+        n = 10
+        p = self.config.state_size[0]
+
+        #Scale weight initialization
+        alpha0 = np.sqrt(2.0/p)
+        alpha1 = np.sqrt(2.0/m)
+        alpha2 = np.sqrt(2.0/j)
+        alpha3 = 1
+
+        #Plus one for bias terms
+        A = tf.Variable(rng.randn(p+1,m)*alpha0, name="hidden_weights", dtype=tf.float32)
+        W1 = tf.Variable(rng.randn(m+1,j)*alpha1, name="hidden_weights2", dtype=tf.float32)
+        W2 = tf.Variable(rng.randn(j+1,n)*alpha2, name="output_weights", dtype=tf.float32)
+        B1 = tf.Variable(rng.randn(m+1,j)*alpha1, name="feedback_weights1", dtype=tf.float32)
+        B2 = tf.Variable(rng.randn(j+1,n)*alpha2, name="feedback_weights2", dtype=tf.float32)
+
+        # network architecture with ones added for bias terms
+        e0 = tf.ones([self.config.batch_size, 1], tf.float32)
+        e1 = tf.ones([self.config.batch_size, 1], tf.float32)
+        x_aug = tf.concat([self.x, e0], 1)
+        h1 = tf.sigmoid(tf.matmul(x_aug, A))
+        h1_aug = tf.concat([h1, e1], 1)
+        h2 = tf.sigmoid(tf.matmul(h1_aug, W1))
+        h2_aug = tf.concat([h2, e1], 1)
+        y_p = tf.matmul(h2_aug, W2)
+
+        with tf.name_scope("loss"):
+            #mean squared error
+            #cost = tf.reduce_sum(tf.pow(y_p-self.y, 2))/2/self.config.batch_size
+            self.loss = tf.reduce_sum(tf.pow(y_p-self.y, 2))/2
+            grad_W2 = tf.gradients(xs=W2, ys=self.loss)[0]
+            grad_W1 = tf.gradients(xs=W1, ys=self.loss)[0]
+            grad_A = tf.gradients(xs=A, ys=self.loss)[0]
+            new_W2 = W2.assign(W2 - self.config.learning_rate*grad_W2)
+            new_W1 = W1.assign(W1 - self.config.learning_rate*grad_W1)
+            new_A = A.assign(A - self.config.learning_rate*grad_A)            
+            self.train_step = [new_W2, new_W1, new_A]
+            correct_prediction = tf.equal(tf.argmax(y_p, 1), tf.argmax(self.y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    def init_saver(self):
+        # here you initialize the tensorflow saver that will be used in saving the checkpoints.
+        self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
+
 class FAModel4(BaseModel):
     #Four layers version
     def __init__(self, config):
         super(FAModel4, self).__init__(config)
         self.build_model()
-        self.init_saver()
+        #self.init_saver()
 
     def build_model(self):
         self.is_training = tf.placeholder(tf.bool)
@@ -241,6 +300,8 @@ class FAModel4(BaseModel):
         # set initial feedforward and feedback weights
         m = 512
         j = 200
+        #m = 50
+        #j = 20
         n = 10
         p = self.config.state_size[0]
 
@@ -368,6 +429,124 @@ class FAModel4(BaseModel):
 
             self.training_metric_tags.append('error_fa_%d'%(idx+1))
             self.training_metrics.append(error_fa)
+
+    def init_saver(self):
+        # here you initialize the tensorflow saver that will be used in saving the checkpoints.
+        self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
+
+
+class FAModel4_Small(BaseModel):
+    #Four layers version
+    def __init__(self, config):
+        super(FAModel4_Small, self).__init__(config)
+        self.build_model()
+        #self.init_saver()
+
+    def build_model(self):
+        self.is_training = tf.placeholder(tf.bool)
+        self.x = tf.placeholder(tf.float32, shape=[None] + self.config.state_size)
+        self.y = tf.placeholder(tf.float32, shape=[None, 10])
+
+        # set initial feedforward and feedback weights
+        m = 50
+        j = 20
+        n = 10
+        p = self.config.state_size[0]
+
+        #Scale weight initialization
+        alpha0 = np.sqrt(2.0/p)
+        alpha1 = np.sqrt(2.0/m)
+        alpha2 = np.sqrt(2.0/j)
+        alpha3 = 1
+
+        #Plus one for bias terms
+        A = tf.Variable(rng.randn(p+1,m)*alpha0, name="hidden_weights", dtype=tf.float32)
+        W1 = tf.Variable(rng.randn(m+1,j)*alpha1, name="hidden_weights2", dtype=tf.float32)
+        W2 = tf.Variable(rng.randn(j+1,n)*alpha2, name="output_weights", dtype=tf.float32)
+        B1 = tf.Variable(rng.randn(m+1,j)*alpha1, name="feedback_weights1", dtype=tf.float32)
+        B2 = tf.Variable(rng.randn(j+1,n)*alpha2, name="feedback_weights2", dtype=tf.float32)
+
+        # network architecture with ones added for bias terms
+        e0 = tf.ones([self.config.batch_size, 1], tf.float32)
+        e1 = tf.ones([self.config.batch_size, 1], tf.float32)
+        x_aug = tf.concat([self.x, e0], 1)
+        h1 = tf.sigmoid(tf.matmul(x_aug, A))
+        h1_aug = tf.concat([h1, e1], 1)
+        h2 = tf.sigmoid(tf_matmul_r(h1_aug, W1, B1))
+        h2_aug = tf.concat([h2, e1], 1)
+        y_p = tf_matmul_r(h2_aug, W2, B2)
+
+        with tf.name_scope("loss"):
+            #mean squared error
+            #cost = tf.reduce_sum(tf.pow(y_p-self.y, 2))/2/self.config.batch_size
+            self.loss = tf.reduce_sum(tf.pow(y_p-self.y, 2))/2
+            grad_W2 = tf.gradients(xs=W2, ys=self.loss)[0]
+            grad_W1 = tf.gradients(xs=W1, ys=self.loss)[0]
+            grad_A = tf.gradients(xs=A, ys=self.loss)[0]
+            e = (y_p - self.y)
+            h1_prime = tf.multiply(h1_aug, 1-h1_aug)[:,0:m]
+            h2_prime = tf.multiply(h2_aug, 1-h2_aug)[:,0:j]
+
+            #FA
+            lmda2 = tf.matmul(e, tf.transpose(B2[0:j,:]))
+            d2 = np.multiply(h2_prime, lmda2)
+
+            new_W2 = W2.assign(W2 - self.config.learning_rate*grad_W2)
+            new_W1 = W1.assign(W1 - self.config.learning_rate*grad_W1)
+            new_A = A.assign(A - self.config.learning_rate*grad_A)            
+            self.train_step = [new_W2, new_W1, new_A]
+            correct_prediction = tf.equal(tf.argmax(y_p, 1), tf.argmax(self.y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+            #Save training metrics
+            Bs = [B1, B2]
+            Ws = [W1, W2]
+            es = [d2, e]
+            dls = []
+            ls = [h1_aug, h2_aug]
+            self._set_training_metrics(es, Bs, Ws)
+
+    # def _set_training_metrics(self, Ws, Bs, es):
+    #     for idx in range(len(Bs)):
+    #         delta_fa = tf.matmul(es[idx], tf.transpose(Bs[idx]))[0,:]
+    #         delta_bp = tf.matmul(es[idx], tf.transpose(Ws[idx]))[0,:]
+    #         alignment = tf.abs(tf_align(delta_fa, delta_bp))
+    #         norm = tf.norm(Ws[idx] - Bs[idx])/tf.norm(Ws[idx])
+    #         sgn_cong = tf.reduce_mean((tf.sign(Ws[idx])*tf.sign(Bs[idx])+1)/2)
+    #         self.training_metric_tags.append('align_B%d'%(idx+2))
+    #         self.training_metrics.append(alignment)
+    #         self.training_metric_tags.append('norm_W%d_B%d'%(idx+2, idx+2))
+    #         self.training_metrics.append(norm)
+    #         self.training_metric_tags.append('sign_cong%d'%(idx+2))
+    #         self.training_metrics.append(sgn_cong)
+
+    #         #delta_fa = tf.matmul(es[idx], tf.transpose(Bs[idx]))[0,:]
+    #         #delta_bp = tf.matmul(es[idx], tf.transpose(Ws[idx]))[0,:]
+    #         #alignment = tf.abs(tf_align(delta_fa, delta_bp))
+
+
+    def _set_training_metrics(self, es, Bs, Ws):
+
+        k = 6
+        #Alignment of B_{i+1}e with dl_i (feedback alignment)
+        for idx in range(len(Bs)):
+            delta_fa = tf.matmul(es[idx], tf.transpose(Bs[idx]))[0,:]
+            delta_bp = tf.matmul(es[idx], tf.transpose(Ws[idx]))[0,:]
+            error_fa = tf.norm(delta_fa - delta_bp)/tf.norm(delta_fa)
+            alignment = tf.abs(tf_align(delta_fa, delta_bp))
+            norm = tf.norm(Ws[idx] - Bs[idx])/tf.norm(Ws[idx])
+            sgn_cong = tf.reduce_mean((tf.sign(Ws[idx])*tf.sign(Bs[idx])+1)/2)
+            # self.training_metric_tags.append('align_B%d'%(idx+1))
+            # self.training_metrics.append(alignment)
+            # self.training_metric_tags.append('error_fa_%d'%(idx+1))
+            # self.training_metrics.append(error_fa)
+            self.training_metric_tags.append('align_B%d'%(idx+2))
+            self.training_metrics.append(alignment)
+            self.training_metric_tags.append('norm_W%d_B%d'%(idx+2, idx+2))
+            self.training_metrics.append(norm)
+            self.training_metric_tags.append('sign_cong%d'%(idx+2))
+            self.training_metrics.append(sgn_cong)
+
 
     def init_saver(self):
         # here you initialize the tensorflow saver that will be used in saving the checkpoints.
