9befaed06d408207685964bfe2f408f98cfe3321
lansdell <ben.lansdell@gmail.com>
2019-09-25 10:40:50 -0500

    Add DAE code

---
+++ mains/3_autoencoder_np_test_dae_goodparam.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env ipython
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]=""
+
+import tensorflow as tf
+import numpy.random as rng
+import numpy as np
+import pickle
+from data_loader.data_generator import MNISTDataGenerator
+from models.npmodels import AENPModel5_DAE
+from trainers.sf_trainer import AESFTrainer
+from utils.config import process_config
+from utils.dirs import create_dirs
+from utils.utils import get_args
+from utils.logger import LoggerNumpy, Logger
+
+def set_hyperparameters(config, attr, vals):
+    for idx, val in enumerate(vals):
+        setattr(config, attr[idx], val)
+
+def main():
+    args = get_args()
+    model_name = 'nodepert_ae5_dae_convparams'
+
+    Model = AENPModel5_DAE
+    Data = MNISTDataGenerator
+    Trainer = AESFTrainer
+
+    config = process_config('./configs/np.json', model_name)
+    create_dirs([config.summary_dir, config.checkpoint_dir])
+
+    #var_vals = [1e-2]
+    N = 1
+    M = 10
+    #M = 1
+    T = config.num_epochs+1
+    n_tags = 13
+    test_losses = np.zeros((N, M))
+    isnan = np.zeros((N, M))
+    metrics = np.zeros((N, M, T, n_tags))
+
+    for n in range(N):
+        tf.reset_default_graph()
+        model = Model(config)
+        data = Data(config)
+        for m in range(M):
+            with tf.Session() as sess:
+                logger = LoggerNumpy(sess, config, model)
+                model.load(sess)
+                trainer = Trainer(sess, model, data, config, logger)
+                try:
+                    trainer.train()
+                except ValueError:
+                    print("Method fails to converge for these parameters")
+                    isnan[n,m] = 1
+                loss, acc = trainer.test()
+                metric = logger.get_data()
+                tags = logger.get_tags()
+                test_losses[n,m] = loss
+                metrics[n,m,:,:] = metric
+        #Save after each run
+        fn = os.path.join(config.summary_dir) + "3_autoencoder_correctbatch.npz"
+        to_save = {
+            'test_losses': test_losses,
+            'metrics': metrics,
+            'isnan': isnan,
+            'tags': tags
+        }
+        pickle.dump(to_save, open(fn, "wb"))
+    return metrics
+
+if __name__ == '__main__':    
+    main()
